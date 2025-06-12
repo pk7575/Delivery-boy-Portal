7db0dca4376754d93da97a3605e57ee4814ae8f0
pk7575 <pradeepseth646@gmail.com>
2025-06-12 11:09:02 +0530

    Update script.js
    Updated script.js with backend integration

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Portal Loaded');
+const BASE_URL_DELIVERY = "https://suriyawan-saffari-backend.onrender.com";
+
+fetch(`${BASE_URL_DELIVERY}/api/delivery/run-sheet`)
+  .then(res => res.json())
+  .then(data => console.log("Delivery Sheet:", data))
+  .catch(err => console.error("Delivery error:", err));
